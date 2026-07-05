7a60c384f44506530758a54fb6e02933c47e838f
Anna Nguyen <annanguyn99@gmail.com>
2026-07-05 22:29:24 +1000

    Set up repo

---
+++ config/sources.py
@@ -0,0 +1,6 @@
+SOURCES = [
+    {
+        "url": "https://www.ato.gov.au/individuals-and-families/income-deductions-offsets-and-records/deductions-you-can-claim/work-related-deductions/working-from-home-expenses/fixed-rate-method",
+        "income_year": "2025-26"
+    }
+]
