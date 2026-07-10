e8b65ff9b2c619a6dfcdb152b44b4f5e112d6984
Anna Nguyen <annanguyn99@gmail.com>
2026-07-10 23:06:17 +1000

    Cache raw data from html pages

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 CACHE_DIR = Path(__file__).resolve().parents[1]
-DATA_DIR = CACHE_DIR / "data"
+DATA_DIR = CACHE_DIR / "raw_data"
--- src/ingestion/fetch.py
+++ src/ingestion/fetch.py
@@ -1,7 +1,14 @@
+import hashlib
+import time
 import requests
 
+from config.config import DATA_DIR
+
 def fetch(url: str) -> str:
-    cache_file = ""
+    cache_file = DATA_DIR / f'{hashlib.sha256(url.encode()).hexdigest()}.html'
+
+    if cache_file.exists():
+        return cache_file.read_text()
 
     res = requests.get(
         url, 
@@ -10,4 +17,15 @@ def fetch(url: str) -> str:
         },
         timeout=30)
     res.raise_for_status()
+
+    cache_file.write_text(res.text)
+
+    time.sleep(2) # each cache miss creates a new request -> without sleep, those requests might hit the server's rate limit
+    
     return res.text
+
+
+if __name__ == "__main__":
+    from config.sources import SOURCES
+    for source in SOURCES:
+        print(fetch(source["url"]), '\n\n')
