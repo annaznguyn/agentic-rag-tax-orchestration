158a92a06249dd90be08038aa762035db8d866f1
annaznguyn <annanguyn99@gmail.com>
2026-07-19 15:13:43 +1000

    Restructured files

--- src/agent/state.py
+++ src/agent/state.py
@@ -1,5 +1,11 @@
 from typing import TypedDict
 
+class Job(TypedDict):
+    occupation: str,
+    income: int,
+    employment_type: str,
+    is_work_from_home: bool
+
 class DeductionItem(TypedDict):
     name: str,
     is_deductible: bool,
