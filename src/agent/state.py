48d0b28d6adac047f5dda4a54860be34a1890ad0
annaznguyn <annanguyn99@gmail.com>
2026-07-19 15:18:34 +1000

    Fixed syntax error

--- src/agent/state.py
+++ src/agent/state.py
@@ -1,15 +1,16 @@
 from typing import TypedDict
 
+
 class Job(TypedDict):
-    occupation: str,
-    income: int,
-    employment_type: str,
+    occupation: str
+    income: int
+    employment_type: str
     is_work_from_home: bool
 
 class DeductionItem(TypedDict):
-    name: str,
-    is_deductible: bool,
-    status: str,
-    ato_context: dict,
-    additional_context: dict,  # additional context from user
-    missing_context: dict,  # missing context, to be asked by agent
+    name: str
+    is_deductible: bool
+    status: str
+    ato_context: dict
+    additional_context: dict  # additional context from user
+    missing_context: dict  # missing context, to be asked by agent
