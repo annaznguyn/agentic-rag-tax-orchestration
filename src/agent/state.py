3677c6dbfedab23ca203f9ada4666e5bc2f44217
annaznguyn <annanguyn99@gmail.com>
2026-07-20 00:29:21 +1000

    Added agent states

---
+++ src/agent/state.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+
+
+class Job(TypedDict):
+    occupation: str
+    income_amount: int
+    employment_type: str  # full time, part time, contract, etc.
+    is_work_from_home: bool
+
+class User(TypedDict):
+    residency_status: str
+    income_year: str
+    has_help_debt: bool
+    jobs: list[Job]
+    extra_info: dict
+
+class DeductionItem(TypedDict):
+    name: str
+    status: str               # todo / missing_info / eligible / ineligible
+    ato_context: list[str]
+    additional_context: dict  # additional context from user
+    missing_context: dict     # missing context, to be asked by agent
+
+# question to be asked by agent
+class PendingQuestion(TypedDict):
+    question: str
+    category: str      # User/Job/DeductionItem
+    ref: str           # which job/deduction item (empty for User)
+    to_be_filled: str  # field to be filled (e.g. employment_type)
+
+class State(TypedDict):
+    user: User
+    deductions: list[DeductionItem]
+    query: str
+    final_responses: list[str]  # list of responses sent to user, excluding agent's questions
+    next: str                   # router's next step
--- src/agent/state/Job.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict
-
-
-class Job(TypedDict):
-    occupation: str
-    income: int
-    employment_type: str
-    is_work_from_home: bool
